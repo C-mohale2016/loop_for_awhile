9d7f739744c6bd6ff924104473e10ba577b51d46
C-mohale <clement.mohale@steltix.com>
2016-11-21 16:50:04 +0200

    displaying car registration from Paarl in a list of string only

---
+++ firstPaarl.js
@@ -0,0 +1,18 @@
+// importing assert module
+const assert = require('assert')
+
+function firstPaarl(location){
+  var carRegNumber = location.split(',');
+
+  for (var i = 0; i < carRegNumber.length; i++)
+
+  if (carRegNumber[i].startsWith('CJ')){
+
+    // console.log(carRegNumber[i]);
+
+  }
+  return carRegNumber[i];
+}
+
+firstPaarl('CA 448-225,CY 788-422,CJ 334-445');
+assert.equal(firstPaarl('carRegNumber[i]'));
