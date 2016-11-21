e352abf58c8e93a1e668569496acc478f8ad12a6
C-mohale <clement.mohale@steltix.com>
2016-11-21 10:49:49 +0200

    modified the file by importing assert module

--- shoppingList.js
+++ shoppingList.js
@@ -1,7 +1,10 @@
+// importing assert module
+const assert = require('assert');
+
 function shoppingList(grocery){
   var foodList = grocery.split(',');
   for (var i = 0; i < foodList.length; i++) {
-    console.log(foodList[i]);
+    // console.log(foodList[i].trim());
 
   }
 
@@ -9,3 +12,4 @@ function shoppingList(grocery){
 }
 
 shoppingList('1x bread, 6x sausages, 2x 2L Coke');
+assert.equal(shoppingList('foodList[i]'));
