334e7ea93dbd69b41d37a27dad2399e842b09d01
C-mohale <clement.mohale@steltix.com>
2016-11-21 15:23:39 +0200

    created each registration number to displays in a list

--- displayEachRegNumber.js
+++ displayEachRegNumber.js
@@ -4,10 +4,12 @@ const assert = require('assert')
 function displayEachRegNumber(regNum){
   var carRegNumber = regNum.split(',');
   for (var i = 0; i < carRegNumber.length; i++){
-    console.log(carRegNumber[i].trim());
+    // console.log(carRegNumber[i].trim());
 
   }
   return carRegNumber[i];
 }
+// calling my function
 displayEachRegNumber('CA 449462, CJ 775762, CY 12244');
+// defining my assert
 assert.equal(displayEachRegNumber('carRegNumber[i]'));
