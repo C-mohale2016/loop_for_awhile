dc1d75b86625edd5472d939fdc79d69a5452007b
C-mohale <clement.mohale@steltix.com>
2016-11-22 14:46:19 +0200

    file modified with a curly braces on the for loop line

--- firstPaarl.js
+++ firstPaarl.js
@@ -4,14 +4,15 @@ const assert = require('assert')
 function firstPaarl(location){
   var carRegNumber = location.split(',');
 
-  for (var i = 0; i < carRegNumber.length; i++)
+  for (var i = 0; i < carRegNumber.length; i++){
+    if (carRegNumber[i].startsWith('CJ')){
 
-  if (carRegNumber[i].startsWith('CJ')){
-
-    // console.log(carRegNumber[i]);
+      // console.log(carRegNumber[i]);
+      return carRegNumber[i];
+    }
 
   }
-  return carRegNumber[i];
+
 }
 
 firstPaarl('CA 448-225,CY 788-422,CJ 334-445');
