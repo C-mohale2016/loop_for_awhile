03a4cdf54c115ef7458ece551521055dc0970cc0
C-mohale <clement.mohale@steltix.com>
2016-11-24 10:20:39 +0200

    used assert.equal to test whether the value stored its the longest word

---
+++ longestWord.js
@@ -0,0 +1,15 @@
+function longestWord(string) {
+    var str = string.split(" ");
+    var longest = 0;
+    var word = null;
+    for (var i = 0; i < str.length - 1; i++) {
+        if (longest < str[i].length) {
+            longest = str[i].length;
+            word = str[i];
+        }
+    }
+    return word;
+}
+const assert = require('assert');
+var longWord = longestWord("codeX prides in their coders");
+assert.equal(longWord, "prides");
